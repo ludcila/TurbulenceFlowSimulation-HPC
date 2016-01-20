6470ae8ab7d0cf067874982eef5e2d11f81cb16b
Raffo-o <raffone92@gmail.com>
2016-01-20 18:46:32 +0100

    3d changes

--- stencils/KepsObstacleStencil.cpp
+++ stencils/KepsObstacleStencil.cpp
@@ -42,5 +42,35 @@ void KepsObstacleStencil::apply ( TurbulentFlowField & flowField, int i, int j){
 
 void KepsObstacleStencil::apply ( TurbulentFlowField & flowField, int i, int j, int k){
 
+	const int obstacle = flowField.getFlags().getValue(i, j, k);
+
+	if ((obstacle & OBSTACLE_SELF) == 1){
+	
+		/* if top cell is fluid */
+		if ((obstacle & OBSTACLE_TOP) == 0){
+			flowField.getKineticEnergy().getScalar(i, j, k) = 0;
+			flowField.getTurbulentViscosity().getScalar(i, j, k) = 0;
+			flowField.getDissipationRate().getScalar(i, j, k) = flowField.getDissipationRate().getScalar(i, j+1, k);
+		}
+		/* if bottom cell is fluid */
+		if ((obstacle & OBSTACLE_BOTTOM) == 0){
+			flowField.getDissipationRate().getScalar(i, j, k) = flowField.getDissipationRate().getScalar(i, j-1, k);
+		}
+		/* if right cell is fluid */
+		if ((obstacle & OBSTACLE_RIGHT) == 0){
+			flowField.getKineticEnergy().getScalar(i, j, k) = 0;
+			flowField.getTurbulentViscosity().getScalar(i, j, k) = 0;
+			flowField.getDissipationRate().getScalar(i, j, k) = flowField.getDissipationRate().getScalar(i+1, j,k);
+		}
+		/* if left cell is fluid */
+		if ((obstacle & OBSTACLE_LEFT) == 0){
+			flowField.getDissipationRate().getScalar(i, j, k) = flowField.getDissipationRate().getScalar(i-1, j, k);
+		}
+		/* if top and right cells are fluid (top-right corner)*/
+		if ((obstacle & OBSTACLE_TOP) == 0 && (obstacle & OBSTACLE_RIGHT) == 0){
+			flowField.getDissipationRate().getScalar(i, j, k) = (flowField.getDissipationRate().getScalar(i+1, j, k) + flowField.getDissipationRate().getScalar(i, j+1, k)) * 0.5;
+		}
+		
+	}
 
 }
